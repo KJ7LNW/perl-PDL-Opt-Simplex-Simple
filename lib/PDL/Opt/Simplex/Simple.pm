55360d6d18d4e96ad69463871d567b4c58770d08
Eric Wheeler <simplex-simple@z.ewheeler.org>
2022-06-10 14:02:46 -0700

    Added GPL3 header

--- lib/PDL/Opt/Simplex/Simple.pm
+++ lib/PDL/Opt/Simplex/Simple.pm
@@ -1,5 +1,27 @@
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#  Copyright (C) 2021- eWheeler, Inc. L<https://www.linuxglobal.com/>
+#  Originally written by Eric Wheeler, KJ7LNW
+#  All rights reserved.
+#
+#  All tradmarks, product names, logos, and brands are property of their
+#  respective owners and no grant or license is provided thereof.
+
 			
 package PDL::Opt::Simplex::Simple;
+$VERSION = '1.1';
 
 use strict;
 use warnings;
